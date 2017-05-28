4af52458f0aea4fe658cf392529f6ae031fd0182
kkke <kkke@nwsuaf.edu.cn>
2017-05-28 16:12:33 +0800

    update example

--- test.cpp
+++ test.cpp
@@ -2,6 +2,7 @@
 #include <cstring>
 #include <iostream>
 #include <algorithm>
+#include <cmath>
 #include "SColor.h"
 
 using namespace std;
@@ -11,9 +12,14 @@ int main()
 	SColor defaultColor;
 	SColor ac(SColor::BLUE,SColor::RED,SColor::UNDERLINE|SColor::ITALIC);
 	SColor::hideCursor();
-	while(1)
+	int maxx=5,maxy=8;
+	for(int i=0,j=0;;i++,j++)
 	{
+		int x=i,y=j;
+		while(x>maxx)x=abs(2*maxx-x);
+		while(y>maxy)y=abs(2*maxy-y);
 		SColor::clean();
+		SColor::setCursor(x,y*2);
 		cout<<ac<<"test"<<defaultColor<<endl;
 		ac^=SColor::INVERT;
 		for(int i=0;i<100000000;i++);
